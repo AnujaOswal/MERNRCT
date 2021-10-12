afd17020a110cc8284ba348ac02b40ff36809e3c
AnujaOswal <Oswalanujak@gmail.com>
2021-10-12 21:37:25 +0530

    deploychanges

--- Backend/server.js
+++ Backend/server.js
@@ -3,14 +3,13 @@ import express from "express";
 import router from "./routes/recipe.js";
 import { userRouter } from "./routes/user.js";
 import dotenv from 'dotenv'
-
 import connectDB from './config/db.js'
-dotenv.config({ path : "./config/.env" });
 
-connectDB()
+dotenv.config();
+
+connectDB();
 
 const app = express()
-const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
@@ -24,9 +23,9 @@ app.use("/recipes", router);
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'))
+  app.use(express.static('Frontend/build'))
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'Frontend', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
@@ -34,6 +33,7 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
+const PORT = process.env.PORT || 5000
 
 
 app.listen(
